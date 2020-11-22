6e82db554b9d39dbbffc6346943ab4e4c1426bf8
Andrew Nguyen <anguy95@gmail.com>
2020-11-22 14:05:47 -0800

    initial push

---
+++ src/scripts/app.js
@@ -0,0 +1,26 @@
+import { picoapp } from 'picoapp'
+
+import header from '@/components/header.js'
+import productSelection from '@/components/product-selection.js'
+import cartDrawer from '@/components/cartDrawer.js'
+import cartDrawerItem from '@/components/cartDrawerItem.js'
+import accountLogin from '@/components/accountLogin.js'
+import product from '@/components/product.js'
+import productCounter from '@/components/product-counter.js'
+
+const state = {
+  cartOpen: false
+}
+
+const components = {
+  slaterWelcome,
+  accountLogin,
+  header,
+  productSelection,
+  cartDrawer,
+  cartDrawerItem,
+  product,
+  productCounter
+}
+
+export default picoapp(components, state)
